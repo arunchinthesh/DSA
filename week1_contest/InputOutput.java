a71e40d6512c7d321343aa34012b9ef0b595d58f
Arun Chinthesh <chinthesharun@gmail.com>
2026-06-17 06:22:47 +0530

    InputOutput.java

---
+++ week1_contest/InputOutput.java
@@ -0,0 +1,28 @@
+/*Write a program which takes the following input and prints them one by one. 1. An whole number 2. A letter 3. A number with fractional part 4. A word
+Input Format
+34 S 56.1 PowerRanger
+Constraints
+2 ≤ whole numer ≤ 10^6
+Output Format
+34
+S
+56.1
+PowerRanger
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int whole=s.nextInt();
+        char letter=s.next().charAt(0);
+        double fraction=s.nextDouble();
+        s.nextLine();
+        String word=s.nextLine();
+System.out.println(whole);
+System.out.println(letter);
+System.out.println(fraction);
+System.out.println(word);
+
+        
+    }
+}
