cb65e2ddff60a7d40a4e0857fcb3f1961919d008
Arun Chinthesh <chinthesharun@gmail.com>
2026-06-17 06:24:58 +0530

    PrintNnumbers.java

---
+++ week1_contest/PrintNnumbers.java
@@ -0,0 +1,18 @@
+/*Write a program to print N numbers starting from 2. Take N as input and print from 2 to N;
+Input Format
+6
+Constraints
+3<=N<=10^3
+Output Format
+2 3 4 5 6 (the output should exactly match)
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner s=new Scanner(System.in);
+      int n=s.nextInt();
+      for(int i=2;i<=n;i++){
+        System.out.print(i+" ");
+      }
+    }
+}
