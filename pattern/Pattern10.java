34b7aea50f39da329f2ea62b4e3229612e16f0b7
Arun Chinthesh <chinthesharun@gmail.com>
2026-06-20 16:13:09 +0530

    Pyramid Pattern10.java

---
+++ pattern/Pattern10.java
@@ -0,0 +1,31 @@
+/*
+Given a number n, print pyramid pattern with n lines.
+Examples:
+Input: n = 4
+
+Output:
+    *
+   ***
+  *****
+ *******
+*/
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+    }
+    for(int j=1;j<=i*2-1;j++){
+        System.out.print("*");
+    }
+    System.out.println();
+}
+
+        sc.close();
+    }
+}
