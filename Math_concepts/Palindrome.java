04a6994f2286889c8d1bb3bef7ebb84b907ee754
Arun Chinthesh <chinthesharun@gmail.com>
2026-06-26 08:56:04 +0530

    week3 Palindrome.java

---
+++ Math_concepts/Palindrome.java
@@ -0,0 +1,20 @@
+//use Math.abs(n) for negative value.
+class Palindrome {
+    public boolean isPalindrome(int n) {
+        n=Math.abs(n);
+        int rev=0;
+        int original=n;
+     while(n>0){
+         rev=rev*10+(n%10);
+         n=n/10;
+     } 
+
+     if(rev==original){
+         return true;
+     }
+     else{
+         return false;
+     }
+        
+    }
+}
