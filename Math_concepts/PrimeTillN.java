6986c4ba8c11aed9244b14d7aa67174d377fde65
Arun Chinthesh <chinthesharun@gmail.com>
2026-06-27 06:32:38 +0530

    week 3

---
+++ Math_concepts/PrimeTillN.java
@@ -0,0 +1,19 @@
+//Write a program that gets n as input and print all the prime numbers till that number.
+class Main {
+    public static void main(String[] args) {
+int n=10;
+for(int i=2;i<=n;i++){
+boolean prime=true;
+    for(int j=2;j<=i/2;j++){
+        if(i%j==0){
+            prime=false;
+        }
+    }
+    if(prime){
+    System.out.print(i+" ");
+}
+
+}
+
+    }
+}
