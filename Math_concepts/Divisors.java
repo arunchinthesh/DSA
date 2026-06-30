3ca56a37a65416266f74ffc3b2039e3c6868b4b9
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-01 05:14:36 +0530

    week 3

--- Math_concepts/Divisors.java
+++ Math_concepts/Divisors.java
@@ -1,7 +1,8 @@
 class Solution {
     public ArrayList<Integer> getDivisors(int n) {
         ArrayList<Integer> divisor=new ArrayList<Integer>();
-        for(int i=1;i*i<=n;i++){                             //i*i because we r considering root n meaning the loop will run only to its half hence reduce time complexity
+        for(int i=1;i*i<=n;i++){     //i*i because we r considering root n meaning the loop will run only to its half hence reduce time complexity
+                                     //we can also use Math.sqrt(n) instead of i*i
             if(n%i==0){
                 divisor.add(i);
                 if(i!=n/i){                                 //to check if the divident and divisors are the same that is 6*6=36           
