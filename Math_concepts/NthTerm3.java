8fd218699072320e2d4e138ee9640ec5e68ba218
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-05 11:22:56 +0530

    Nth term 2

--- Math_concepts/NthTerm3.java
+++ Math_concepts/NthTerm3.java
@@ -1,12 +1,10 @@
 //1+(1+3)+(1+3+5)
-class NthTerm3 {
+class Main {
     public static void main(String[] args) {
-int n=3,series=0;
+int n=4,sum=0;
 for(int i=1;i<=n;i++){
-    int sum=0;
     sum=sum+(i*i);
-    series+=sum;
 }
-System.out.println(series);
+System.out.println(sum);
 }
 }
