bff250459391c68904ce75aca6af7026120fd505
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-05 16:40:58 +0530

    Array Traversal
    Refactored arrayTraversal method to use enhanced for loop.

---
+++ Math_concepts/ArrayTraversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            System.out.print(" "+arr[i]);
+        }
+        
+    }
+}
+
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        for(int x:arr){
+            System.out.print(" "+x);
+        }
+        
+    }
+}
