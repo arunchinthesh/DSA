012f00bc2aaeee47b2727b46ea78d1095f053776
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-05 12:12:43 +0530

    reverse number

--- Math_concepts/Reverse.java
+++ Math_concepts/Reverse.java
@@ -1,3 +1,14 @@
+/*
+You are given an integer n. Your task is to reverse the digits, ensuring that the reversed number has no leading zeroes.
+Examples:
+Input: n = 122
+Output: 221
+Explanation: By reversing the digits of number, number will change into 221.
+
+Input : n = 200
+Output: 2
+Explanation: By reversing the digits of number, number will change into 2.
+*/
 class Reverse {
     public static void main(String[] args) {
         int n=12345;
