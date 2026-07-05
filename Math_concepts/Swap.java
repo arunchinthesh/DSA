f5ac2e650efe2f4bf8b5c72277812f6c9ccded9e
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-05 12:29:46 +0530

    swap 2 numbers

---
+++ Math_concepts/Swap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Swap{
+  public static void main(String[] args){
+    int a=2,b=10,temp=0;
+    temp=a;
+    a=b;
+    b=temp;
+    System.out.println("a = "+a);
+    System.out.println("b = "+b);
+  }
+}
