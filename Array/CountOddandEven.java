141b7434c861ac4a247d429dd26d673d6b9e94cd
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-09 04:47:15 +0530

    Count Odd and Even
    Implement a method to count odd and even numbers in an array.

---
+++ Array/CountOddandEven.java
@@ -0,0 +1,17 @@
+//since it is int it should return the results
+
+class CountOddandEven {
+    public int[] countOddEven(int[] arr) {
+        int countodd=0,counteven=0;
+        int result[]=new int[2];
+        for(int x:arr){
+            if(x%2==0){
+                ++result[1];
+            }
+            else{
+                ++result[0];
+            }
+        }
+return result;
+    }
+}
