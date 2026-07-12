b374ea3dba421d61765ca7afc413b6164ccf7d7d
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-12 11:58:12 +0530

    Sieve Of Eratosthenes
    Removed commented-out code that initializes array values to 0.

--- Array/SieveOfEratosthenes.java
+++ Array/SieveOfEratosthenes.java
@@ -2,9 +2,10 @@ class Solution {
     public ArrayList<Integer> sieve(int n) {
     ArrayList<Integer> result =new ArrayList<>();
     int[] arr=new int[n+1];
-    for(int i=0;i<n;i++){
+/*    for(int i=0;i<n;i++){       by default java set its array values to 0
         arr[i]=0;
     }
+*/
     for(int i=2;i<=Math.sqrt(n);i++){
         for(int j=i*2;j<=n;j=j+i){
             arr[j]=1;
