83252b6a638c568a99450f70628fbad271a9700b
Arun Chinthesh <chinthesharun@gmail.com>
2026-07-15 05:11:29 +0530

    1 to n Without Loops

--- Recursion/Print1Ton.java
+++ Recursion/Print1Ton.java
@@ -1,4 +1,17 @@
 class Solution {
+    public void printTillN(int n) {
+        if(n==0){
+            return;
+        }
+        else{
+            printTillN(n-1);
+            System.out.print(" "+n);
+        }
+        
+         }
+}
+
+class Solution {
     int count=1;
     public void printTillN(int n) {
         if(count<=n){
